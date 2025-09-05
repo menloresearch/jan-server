1796bac2a62609689dc8c359c5a24cc1b60bc0e2
jjchen01 <jinjie@menlo.ai>
2025-09-05 12:30:24 +0800

    /jan/v1/models

--- apps/jan-api-gateway/application/app/interfaces/http/routes/jan/v1/v1.go
+++ apps/jan-api-gateway/application/app/interfaces/http/routes/jan/v1/v1.go
@@ -1,11 +1,16 @@
 package v1
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	inferencemodel "menlo.ai/jan-api-gateway/app/domain/inference_model"
+	inferencemodelregistry "menlo.ai/jan-api-gateway/app/domain/inference_model_registry"
 	"menlo.ai/jan-api-gateway/app/interfaces/http/routes/jan/v1/auth"
 	"menlo.ai/jan-api-gateway/app/interfaces/http/routes/jan/v1/chat"
 	"menlo.ai/jan-api-gateway/app/interfaces/http/routes/jan/v1/conversations"
 	"menlo.ai/jan-api-gateway/app/interfaces/http/routes/jan/v1/organization"
+	"menlo.ai/jan-api-gateway/app/utils/functional"
 )
 
 type V1Route struct {
@@ -34,4 +39,41 @@ func (v1Route *V1Route) RegisterRouter(router gin.IRouter) {
 	v1Route.chat.RegisterRouter(v1Router)
 	v1Route.conversations.RegisterRouter(v1Router)
 	v1Route.organizations.RegisterRouter(v1Router)
+	v1Router.GET("/models", v1Route.GetModels)
+}
+
+// ListModels
+// @Summary List available models
+// @Description Retrieves a list of available models that can be used for chat completions or other tasks.
+// @Tags Jan, Jan-Models
+// @Security BearerAuth
+// @Accept json
+// @Produce json
+// @Success 200 {object} ModelsResponse "Successful response"
+// @Router /jan/v1/models [get]
+func (v1Route *V1Route) GetModels(reqCtx *gin.Context) {
+	registry := inferencemodelregistry.GetInstance()
+	reqCtx.JSON(http.StatusOK, ModelsResponse{
+		Object: "list",
+		Data: functional.Map(registry.ListModels(), func(model inferencemodel.Model) Model {
+			return Model{
+				ID:      model.ID,
+				Object:  model.Object,
+				Created: model.Created,
+				OwnedBy: model.OwnedBy,
+			}
+		}),
+	})
+}
+
+type Model struct {
+	ID      string `json:"id"`
+	Object  string `json:"object"`
+	Created int    `json:"created"`
+	OwnedBy string `json:"owned_by"`
+}
+
+type ModelsResponse struct {
+	Object string  `json:"object"`
+	Data   []Model `json:"data"`
 }
